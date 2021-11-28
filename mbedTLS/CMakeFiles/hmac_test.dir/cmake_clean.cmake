daf34c03c4ab65246938fdfe49a6ba327b714efe
“ding2zz” <543223240@qq.com>
2021-11-28 19:49:28 +0800

    mbedTLS start
    Signed-off-by: “ding2zz” <543223240@qq.com>

---
+++ mbedTLS/CMakeFiles/hmac_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hmac_test.dir/hmac_test.c.o"
+  "hmac_test"
+  "hmac_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hmac_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
